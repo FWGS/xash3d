441286cc6c1b20e0e31b4878133a069fecfa4f05
Alibek Omarov <a1ba.omarov@gmail.com>
2015-02-08 20:21:04 +0600

    [engine] Portability header with windows types

---
+++ common/port.h
@@ -0,0 +1,27 @@
+#ifndef PORT_H
+#define PORT_H
+
+#include <limits.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef unsigned char	    BYTE;
+typedef short int	    WORD;
+typedef unsigned int	    DWORD;
+typedef long int	    LONG;
+typedef unsigned long int   ULONG;
+
+typedef void* HANDLE;
+typedef void* HMODULE;
+typedef void* HINSTANCE;
+
+typedef char* LPSTR;
+
+typedef struct POINT_s
+{
+    int x;
+    int y;
+} POINT;
+
+#endif
