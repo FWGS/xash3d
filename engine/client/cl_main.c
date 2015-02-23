1d739c9ac2b8a8dffb3f4bae26adac290ba3a362
Alibek Omarov <a1ba.omarov@gmail.com>
2015-02-23 22:32:12 +0600

    Yet another fix to hide GCC error

--- engine/client/cl_main.c
+++ engine/client/cl_main.c
@@ -1317,8 +1317,7 @@ void CL_ConnectionlessPacket( netadr_t from, sizebuf_t *msg )
 			return;
 		}
 
-		ShowWindow( host.hWnd, SW_RESTORE );
-		SetForegroundWindow ( host.hWnd );
+		SDL_RestoreWindow( host.hWnd );
 		args = BF_ReadString( msg );
 		Cbuf_AddText( args );
 		Cbuf_AddText( "\n" );
@@ -1511,7 +1510,7 @@ A file has been received via the fragmentation/reassembly layer, put it in the r
  see if we have finished downloading files.
 ====================
 */
-void CL_ProcessFile( BOOL successfully_received, const char *filename )
+void CL_ProcessFile( qboolean successfully_received, const char *filename )
 {
 	MsgDev( D_INFO, "Received %s, but file processing is not hooked up!!!\n", filename );
 
