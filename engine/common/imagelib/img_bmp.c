3321d6351b97d38ba53e5ae265219179698158bb
Alibek Omarov <a1ba.omarov@gmail.com>
2015-02-23 21:00:15 +0600

    Add struct defintions from Windows for BMP

--- engine/common/imagelib/img_bmp.c
+++ engine/common/imagelib/img_bmp.c
@@ -15,6 +15,38 @@ GNU General Public License for more details.
 
 #include "imagelib.h"
 
+#ifndef _WIN32
+#define BI_RGB 0
+typedef struct tagRGBQUAD {
+	BYTE rgbBlue;
+	BYTE rgbGreen;
+	BYTE rgbRed;
+	BYTE rgbReserved;
+} RGBQUAD;
+
+typedef struct tagBITMAPFILEHEADER {
+	WORD  bfType;
+	DWORD bfSize;
+	WORD  bfReserved1;
+	WORD  bfReserved2;
+	DWORD bfOffBits;
+} BITMAPFILEHEADER, *PBITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFOHEADER {
+	DWORD biSize;
+	LONG  biWidth;
+	LONG  biHeight;
+	WORD  biPlanes;
+	WORD  biBitCount;
+	DWORD biCompression;
+	DWORD biSizeImage;
+	LONG  biXPelsPerMeter;
+	LONG  biYPelsPerMeter;
+	DWORD biClrUsed;
+	DWORD biClrImportant;
+} BITMAPINFOHEADER, *PBITMAPINFOHEADER;
+#endif
+
 /*
 =============
 Image_LoadBMP
