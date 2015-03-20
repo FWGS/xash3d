1e8445e50c9c066e7800bf72bb6a985eabe054d0
Alibek Omarov <a1ba.omarov@gmail.com>
2015-03-20 22:57:29 +0600

    Add android game launcher and android NDK makefile

---
+++ engine/Android.mk
@@ -0,0 +1,16 @@
+/*
+android.cpp -- Run Xash Engine on Android
+nicknekit
+*/
+
+#define GAME_PATH	"valve"	// default dir to start from
+
+#include "common.h"
+#include <android/log.h>
+
+int main( int argc, char **argv )
+{
+    __android_log_print(ANDROID_LOG_DEBUG,"Xash","Starting xash engine...");
+	return Host_Main( GAME_PATH, false, NULL );
+}
+
