856d4b02ac1f847357602ad189001a4192e0d888
Alibek Omarov <a1ba.omarov@gmail.com>
2015-03-26 23:05:56 +0600

    include sys/select.h

--- engine/common/network.c
+++ engine/common/network.c
@@ -22,6 +22,7 @@ GNU General Public License for more details.
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+#include <sys/select.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
