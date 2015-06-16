ce4f2c183db11f41c8ff63c6d37a00b2e837e18d
mittorn <mittorn@sibmail.com>
2015-06-16 22:09:03 +0700

    Put aapcs attributes under SOFTFP_LINK

--- engine/platform/android/android-touchif.cpp
+++ engine/platform/android/android-touchif.cpp
@@ -573,6 +573,13 @@ JAVA_FUNC(initTouchControls) ( JNIEnv* env,	jobject thiz,jstring graphics_dir,ji
 	return 0;
 }
 
+#ifdef SOFTFP_LINK
+void EXPORT_ME
+JAVA_FUNC(setTouchSettings) (JNIEnv *env, jobject obj,	jfloat alpha,jfloat strafe,jfloat fwd,jfloat pitch,jfloat yaw,int other) __attribute__((pcs("aapcs")));
+void EXPORT_ME
+JAVA_FUNC(touchEvent) (JNIEnv *env, jobject obj,jint action, jint pid, jfloat x, jfloat y) __attribute__((pcs("aapcs")));
+#endif
+
 
 jint EXPORT_ME
 JAVA_FUNC(frame) ( JNIEnv* env,	jobject thiz )
@@ -597,8 +604,6 @@ JAVA_FUNC(keypress) (JNIEnv *env, jobject obj,jint down, jint keycode, jint unic
 	PortableKeyEvent(down,keycode,unicode);
 }
 
-void EXPORT_ME
-JAVA_FUNC(touchEvent) (JNIEnv *env, jobject obj,jint action, jint pid, jfloat x, jfloat y) __attribute__((pcs("aapcs")));
 void EXPORT_ME
 JAVA_FUNC(touchEvent) (JNIEnv *env, jobject obj,jint action, jint pid, jfloat x, jfloat y)
 {
@@ -643,8 +648,6 @@ JAVA_FUNC(analogYaw) (JNIEnv *env, jobject obj,	jint mode,jfloat v)
 {
 	PortableLookYaw(mode, v);
 }
-void EXPORT_ME
-JAVA_FUNC(setTouchSettings) (JNIEnv *env, jobject obj,	jfloat alpha,jfloat strafe,jfloat fwd,jfloat pitch,jfloat yaw,int other) __attribute__((pcs("aapcs")));
 void EXPORT_ME
 JAVA_FUNC(setTouchSettings) (JNIEnv *env, jobject obj,	jfloat alpha,jfloat strafe,jfloat fwd,jfloat pitch,jfloat yaw,int other)
 {
