dfeba2f17a1348edee256650170c0b39763a6aab
a1batross <a1ba.omarov@gmail.com>
2015-08-15 18:41:10 +0600

    Fix dedicated build

--- engine/common/con_utils.c
+++ engine/common/con_utils.c
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 #include "client.h"
 #include "const.h"
 #include "bspfile.h"
-#include "../cl_dll/kbutton.h"
+#include "kbutton.h"
 
 extern convar_t	*con_gamemaps;
 
