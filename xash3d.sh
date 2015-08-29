df3fd8bc4035c1b4441787db55b3d79198ddb118
a1batross <a1ba.omarov@gmail.com>
2015-08-29 21:32:54 +0300

    Script now checks his directory. If it "bin", then GAMEROOT must be "../lib/xash3d" and assign basedir to user's Steam HL directory. Else run in place.

--- xash3d.sh
+++ xash3d.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-GAMEROOT=$(dirname -- $(readlink -f -- $0))
+SCRIPT_DIR=${PWD##*/}
+
+if [ "$SCRIPT_DIR" == "bin" ]; then
+# Xash3D SDL is installed in system, so run it from lib/xash3d/ directory with Steam HL basedir if XASH3D_BASEDIR is not set
+	if [ -z "$XASH3D_BASEDIR" ]; then
+		export XASH3D_BASEDIR="$HOME/.steam/steam/steamapps/common/Half-Life/"
+	fi
+	GAMEROOT=${PWD}/../lib/xash3d
+	echo "Xash3D SDL is installed in system. Running from ${PWD}/../lib/xash3d/"
+else
+	GAMEROOT=$(dirname -- $(readlink -f -- $0))
+fi
 
 #determine platform
 UNAME=`uname`
 if [ "$UNAME" == "Darwin" ]; then
-   # prepend our lib path to LD_LIBRARY_PATH
-   export DYLD_LIBRARY_PATH=${GAMEROOT}:$DYLD_LIBRARY_PATH
+	# prepend our lib path to LD_LIBRARY_PATH
+	export DYLD_LIBRARY_PATH=${GAMEROOT}:$DYLD_LIBRARY_PATH
 elif [ "$UNAME" == "Linux" ]; then
-   # prepend our lib path to LD_LIBRARY_PATH
-   export LD_LIBRARY_PATH=${GAMEROOT}:$LD_LIBRARY_PATH
+	# prepend our lib path to LD_LIBRARY_PATH
+	export LD_LIBRARY_PATH=${GAMEROOT}:$LD_LIBRARY_PATH
 fi
 
 if [ -z $GAMEEXE ]; then
 	if [ "$UNAME" == "Darwin" ]; then
-	  GAMEEXE=xash3d
+		GAMEEXE=xash3d
 	elif [ "$UNAME" == "Linux" ]; then
-	  GAMEEXE=xash3d
+		GAMEEXE=xash3d
 	fi
 fi
 
