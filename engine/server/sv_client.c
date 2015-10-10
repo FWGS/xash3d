870d5f77381265ceb3c97afaaac2364d06d37df7
a1batross <a1ba.omarov@gmail.com>
2015-10-10 13:25:29 +0300

    Revert "Try fix connect problem". It making problems on changelevel.
    This reverts commit 2a78779e6992d9fecd3c8014ac41b7ced066cb2c.

--- engine/server/sv_client.c
+++ engine/server/sv_client.c
@@ -169,7 +169,7 @@ void SV_DirectConnect( netadr_t from )
 	// if there is already a slot for this ip, reuse it
 	for( i = 0, cl = svs.clients; i < sv_maxclients->integer; i++, cl++ )
 	{
-		if( cl->state == cs_free )
+		if( cl->state == cs_free || cl->state == cs_zombie )
 			continue;
 
 		if( NET_CompareBaseAdr( from, cl->netchan.remote_address ) && ( cl->netchan.qport == qport || from.port == cl->netchan.remote_address.port ))
@@ -239,7 +239,6 @@ gotnewcl:
 		SV_DropClient( newcl );
 		return;
 	}
-
 	// get the game a chance to reject this connection or modify the userinfo
 	if( !( SV_ClientConnect( ent, userinfo )))
 	{
@@ -252,6 +251,7 @@ gotnewcl:
 		SV_DropClient( newcl );
 		return;
 	}
+
 	// parse some info from the info strings
 	SV_UserinfoChanged( newcl, userinfo );
 
