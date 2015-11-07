c91e273d11a139bdd19cfea401fce77357cabb3e
mittorn <mittorn@sibmail.com>
2015-11-07 22:59:34 +0600

    Fix custom server weapon animations

--- engine/client/cl_parse.c
+++ engine/client/cl_parse.c
@@ -1617,8 +1617,7 @@ void CL_ParseServerMessage( sizebuf_t *msg )
 		case svc_weaponanim:
 			param1 = BF_ReadByte( msg );	// iAnim
 			param2 = BF_ReadByte( msg );	// body
-			if( !cl_lw->value )
-				CL_WeaponAnim( param1, param2 );
+			CL_WeaponAnim( param1, param2 );
 			break;
 		case svc_bspdecal:
 			CL_ParseStaticDecal( msg );
