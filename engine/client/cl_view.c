81bee2e7fcb5b7056c37ad57ad2dce6b4837b771
Solexid <solexid@gmail.com>
2016-01-26 09:10:51 +0300

    Some codestyle fixes.

--- engine/client/cl_view.c
+++ engine/client/cl_view.c
@@ -317,58 +317,57 @@ void V_CalcRefDef( void )
 		
 		if (host.vrmode)	
 		{
-/*VR mode TRUE
-Temp vars for preivious vector data.
-Need to correct render Right Eye.*/
-		vec3_t tmright;
-		vec3_t tmvieworg;
-		vec3_t tmup;
-		vec3_t tmforward;
-		VectorCopy(cl.refdef.right, tmright);
-		VectorCopy(cl.refdef.forward, tmforward);
-		VectorCopy(cl.refdef.up, tmup);
-		VectorCopy(cl.refdef.vieworg, tmvieworg);
-		//////Left eye	
-		{	vec3_t tmp;
-		VectorScale(cl.refdef.right, -0.2, tmp);
-		VectorAdd(cl.refdef.vieworg, tmp, cl.refdef.vieworg);
-		vec3_t tmp2, tmp3;
-		// Is this the right place to incorporate head and neck model ?
-		VectorScale(cl.refdef.up, 1, tmp2);
-		VectorScale(cl.refdef.forward, 1.5, tmp3);
-		VectorAdd(cl.refdef.vieworg, tmp2, cl.refdef.vieworg);
-		VectorAdd(cl.refdef.vieworg, tmp3, cl.refdef.vieworg);
-		R_RenderFrame(&cl.refdef, true);
+
+	/*VR mode TRUE
+	Temp vars for preivious vector data.
+	Need to correct render Right Eye.*/
+
+			vec3_t tmright;
+			vec3_t tmvieworg;
+			vec3_t tmup;
+			vec3_t tmforward;
+			VectorCopy(cl.refdef.right, tmright);
+			VectorCopy(cl.refdef.forward, tmforward);
+			VectorCopy(cl.refdef.up, tmup);
+			VectorCopy(cl.refdef.vieworg, tmvieworg);
+
+			//Left eye	
+			{
+				vec3_t tmp;
+				VectorScale(cl.refdef.right, -0.2, tmp);
+				VectorAdd(cl.refdef.vieworg, tmp, cl.refdef.vieworg);
+				vec3_t tmp2, tmp3;
+				VectorScale(cl.refdef.up, 1, tmp2);
+				VectorScale(cl.refdef.forward, 1.5, tmp3);
+				VectorAdd(cl.refdef.vieworg, tmp2, cl.refdef.vieworg);
+				VectorAdd(cl.refdef.vieworg, tmp3, cl.refdef.vieworg);
+				R_RenderFrame(&cl.refdef, true);
+			}	
+			//Right eye	
+			{
+				vec3_t tmp;
+				VectorScale(tmright, 0.2, tmp);
+				VectorAdd(tmvieworg, tmp, tmvieworg);
+				vec3_t tmp2, tmp3;
+				VectorScale(tmup, 1, tmp2);
+				VectorScale(tmforward, 1.5, tmp3);
+				VectorAdd(tmvieworg, tmp2, tmvieworg);
+				VectorAdd(tmvieworg, tmp3, cl.refdef.vieworg);
+				cl.refdef.viewport[0] = (cl.refdef.viewport[2]) + (cl.refdef.viewport[0]);
+				RI.viewport[2] = (cl.refdef.viewport[2]) + (cl.refdef.viewport[2]);
+				R_RenderFrame(&cl.refdef, true);
+			}
+		}
+		else 
+		
+		//No Vr mode
+		
+		{
+				R_RenderFrame(&cl.refdef, true);	
 		}
 		
-		
-		
-		
-	//////Right eye	
-		{
-		vec3_t tmp;
-		VectorScale(tmright, 0.2, tmp);
-		VectorAdd(tmvieworg, tmp, tmvieworg);
-		vec3_t tmp2, tmp3;
-		// Is this the right place to incorporate head and neck model ?
-		VectorScale(tmup, 1, tmp2);
-		VectorScale(tmforward, 1.5, tmp3);
-		VectorAdd(tmvieworg, tmp2, tmvieworg);
-		VectorAdd(tmvieworg, tmp3, cl.refdef.vieworg);
-		cl.refdef.viewport[0] = (cl.refdef.viewport[2]) + (cl.refdef.viewport[0]);
-		
-		RI.viewport[2] = (cl.refdef.viewport[2]) + (cl.refdef.viewport[2]);
-	//	cl.refdef.viewangles[0] = cl.refdef.viewangles[0]+10;
-		
-		// calc FOV
-	
-		R_RenderFrame(&cl.refdef, true);
-		}}
-		else 
-/////No Vr mode
-		{
-R_RenderFrame(&cl.refdef, true);	}
 		cl.refdef.onlyClientDraw = false;
+	
 	} while( cl.refdef.nextView );
 
 	// Xash3D extension. draw debug triangles on a server
