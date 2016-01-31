25207e93aaee907b692ce4d76d91072ecc3be574
Night Owl <sepulkarium45@yahoo.com>
2016-01-31 21:48:30 +0500

    Remove useless casts.

--- engine/common/common.c
+++ engine/common/common.c
@@ -274,7 +274,7 @@ byte* COM_LoadFileForMe( const char *filename, int *pLength )
 	COM_FixSlashes( name );
 
 	pfile = FS_LoadFile( name, &iLength, false );
-	if( pLength ) *pLength = (int *)iLength;
+	if( pLength ) *pLength = iLength;
 
 	if( pfile )
 	{
@@ -312,7 +312,7 @@ byte *COM_LoadFile( const char *filename, int usehunk, int *pLength )
 	COM_FixSlashes( name );
 
 	pfile = FS_LoadFile( name, &iLength, false );
-	if( pLength ) *pLength = (int *)iLength;
+	if( pLength ) *pLength = iLength;
 
 	if( pfile )
 	{
