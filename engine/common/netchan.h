994c32e0c167be6d1e94a251a411689e5fb7879c
a1batross <a1ba.omarov@gmail.com>
2016-02-11 21:06:11 +0600

    Oops, mistake of 3a71f67. Change default master server to ms.xash.su

--- engine/common/netchan.h
+++ engine/common/netchan.h
@@ -59,7 +59,7 @@ GNU General Public License for more details.
 //  bytes will be stripped by the networking channel layer
 #define NET_MAX_MESSAGE		PAD_NUMBER(( NET_MAX_PAYLOAD + HEADER_BYTES ), 16 )
 
-#define MASTERSERVER_ADR		"celest.in:27010"
+#define MASTERSERVER_ADR		"ms.xash.su:27010"
 #define PORT_MASTER			27010
 #define PORT_CLIENT			27005
 #define PORT_SERVER			27015
