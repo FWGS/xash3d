2f3a9549257388566514d856ab70b54f810b7bff
mittorn <mittorn@sibmail.com>
2016-03-02 18:00:18 +0000

    Fix shutdown behaviour

--- engine/common/host.c
+++ engine/common/host.c
@@ -1121,7 +1121,7 @@ int EXPORT Host_Main( int argc, const char **argv, const char *progname, int bCh
 	while( !host.crashed && !host.shutdown_issued )
 	{
 #ifdef XASH_SDL
-		while( SDL_PollEvent( &event ) )
+		while( !host.crashed && !host.shutdown_issued && SDL_PollEvent( &event ) )
 			SDLash_EventFilter( &event );
 #endif
 		newtime = Sys_DoubleTime ();
