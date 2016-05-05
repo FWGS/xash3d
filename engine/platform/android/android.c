054f9be0819d9b27f01a3850ff085cb9273e829e
mittorn <mittorn@sibmail.com>
2016-05-05 20:03:06 +0000

    Keyboard support

--- engine/platform/android/android.c
+++ engine/platform/android/android.c
@@ -132,6 +132,44 @@ void Android_Vibrate( float life, char flags )
 #include "nanogl.h" //use NanoGL
 #include <pthread.h>
 #include "common.h"
+#include "input.h"
+#include "client.h"
+static const int s_android_scantokey[] =
+{
+	0,				K_LEFTARROW,	K_RIGHTARROW,	0,				K_ESCAPE,		// 0
+	0,				0,				'0',			'1',			'2',			// 5
+	'3',			'4',			'5',			'6',			'7',			// 10
+	'8',			'9',			'*',			'#',			K_UPARROW,		// 15
+	K_DOWNARROW,	K_LEFTARROW,	K_RIGHTARROW,	K_ENTER,		0,				// 20
+	0,				0,				0,				0,				'a',			// 25
+	'b',			'c',			'd',			'e',			'f',			// 30
+	'g',			'h',			'i',			'j',			'k',			// 35
+	'l',			'm',			'n',			'o',			'p',			// 40
+	'q',			'r',			's',			't',			'u',			// 45
+	'v',			'w',			'x',			'y',			'z',			// 50
+	',',			'.',			K_ALT,			K_ALT,			K_SHIFT,		// 55
+	K_SHIFT,		K_TAB,			K_SPACE,		0,				0,				// 60
+	0,				K_ENTER,		K_BACKSPACE,	'`',			'-',			// 65
+	'=',			'[',			']',			'\\',			';',			// 70
+	'\'',			'/',			'@',			K_KP_NUMLOCK,		0,				// 75
+	0,				'+',			0,				0,				0,				// 80
+	0,				0,				0,				0,				0,				// 85
+	0,				0,				K_PGUP,			K_PGDN,			0,				// 90
+	0,				K_A_BUTTON,		K_B_BUTTON,		K_C_BUTTON,		K_X_BUTTON,		// 95
+	K_Y_BUTTON,		K_Z_BUTTON,		K_LSHOULDER,	K_RSHOULDER,	K_LTRIGGER,		// 100
+	K_RTRIGGER,		K_LSTICK,		K_RSTICK,		K_ESCAPE,		K_ESCAPE,		// 105
+	0,				K_ESCAPE,		K_DEL,			K_CTRL,			K_CTRL,		// 110
+	K_CAPSLOCK,		0,	0,				0,				0,				// 115
+	0,				K_PAUSE,		K_HOME,			K_END,			K_INS,			// 120
+	0,				0,				0,				0,				0,				// 125
+	0,				K_F1,			K_F2,			K_F3,			K_F4,			// 130
+	K_F5,			K_F6,			K_F7,			K_F8,			K_F9,			// 135
+	K_F10,			K_F11,			K_F12,			K_KP_NUMLOCK,		K_KP_INS,			// 140
+	K_KP_END,		K_KP_DOWNARROW,	K_KP_PGDN,		K_KP_LEFTARROW,	K_KP_5,			// 145
+	K_KP_RIGHTARROW,K_KP_HOME,		K_KP_UPARROW,		K_KP_PGUP,		K_KP_SLASH,		// 150
+	0,		K_KP_MINUS,		K_KP_PLUS,		K_KP_DEL,			',',			// 155
+	K_KP_ENTER,		'=',		'(',			')'
+};
 
 jclass gClass;
 JNIEnv *gEnv;
@@ -145,7 +183,7 @@ int gWidth, gHeight;
 	return JNI_VERSION_1_6;
 }*/
 
-#define MAX_EVENTS 64
+#define ANDROID_MAX_EVENTS 64
 #define MAX_FINGERS 10
 typedef enum event_type
 {
@@ -172,9 +210,10 @@ typedef struct finger_s
 
 static struct {
 	pthread_mutex_t mutex;
-	event_t queue[MAX_EVENTS];
+	event_t queue[ANDROID_MAX_EVENTS];
 	volatile int count;
 	finger_t fingers[MAX_FINGERS];
+	char inputtext[256];
 } events = { PTHREAD_MUTEX_INITIALIZER };
 
 #define Android_Lock() pthread_mutex_lock(&events.mutex);
@@ -185,7 +224,7 @@ int IN_TouchEvent( int type, int fingerID, float x, float y, float dx, float dy 
 event_t *Android_AllocEvent()
 {
 	Android_Lock();
-	if( events.count == MAX_EVENTS )
+	if( events.count == ANDROID_MAX_EVENTS )
 		events.count--; //override last event
 	return &events.queue[ events.count++ ];
 }
@@ -204,9 +243,32 @@ void Android_RunEvents()
 			IN_TouchEvent( events.queue[i].type, events.queue[i].arg, events.queue[i].x, events.queue[i].y, events.queue[i].dx, events.queue[i].dy );
 			Msg("T: %i %d %d %f %f %f %f\n", i,  events.queue[i].type, events.queue[i].arg, events.queue[i].x, events.queue[i].y, events.queue[i].dx, events.queue[i].dy );
 		break;
+		case event_key_down:
+			Key_Event( events.queue[i].arg, true );
+			break;
+		case event_key_up:
+			Key_Event( events.queue[i].arg, false );
+			break;
 		}
 	}
 	events.count = 0;
+	for( i = 0; events.inputtext[i]; i++ )
+	{
+		int ch;
+
+		if( !Q_stricmp( cl_charset->string, "utf-8" ) )
+			ch = (unsigned char)events.inputtext[i];
+		else
+			ch = Con_UtfProcessCharForce( (unsigned char)events.inputtext[i] );
+
+		if( !ch )
+			continue;
+
+		Con_CharEvent( ch );
+		if( cls.key_dest == key_menu )
+			UI_CharEvent ( ch );
+	}
+	events.inputtext[0] = 0;
 	Android_Unlock();
 }
 
@@ -269,10 +331,33 @@ void Java_in_celest_xash3d_XashActivity_nativeQuit(JNIEnv* env, jclass cls)
 {
 }
 
-void Java_in_celest_xash3d_XashActivity_nativeKey(JNIEnv* env, jclass cls, jint code)
+void Java_in_celest_xash3d_XashActivity_nativeKey(JNIEnv* env, jclass cls, jint down, jint code)
 {
+	event_t *event;
+
+	if( code >= ( sizeof( s_android_scantokey ) / sizeof( s_android_scantokey[0] ) ) )
+		return;
+
+	event = Android_AllocEvent();
+
+	if( down )
+		event->type = event_key_down;
+	else
+		event->type = event_key_up;
+
+	event->arg = s_android_scantokey[code];
+
+	Android_PushEvent();
 }
 
+void Java_in_celest_xash3d_XashActivity_nativeString(JNIEnv* env, jclass cls, jobject string)
+{
+	char* str = (char *) (*env)->GetStringUTFChars(env, string, NULL);
+	Android_Lock();
+	strncat( events.inputtext, str, 256 );
+	Android_Unlock();
+	(*env)->ReleaseStringUTFChars(env, string, str);
+}
 
 
 void Java_in_celest_xash3d_XashActivity_nativeTouch(JNIEnv* env, jclass cls, jint finger, jint action, jfloat x, jfloat y ) __attribute__((pcs("aapcs")));
@@ -281,9 +366,12 @@ void Java_in_celest_xash3d_XashActivity_nativeTouch(JNIEnv* env, jclass cls, jin
 	float dx, dy;
 	event_t *event;
 
+	// if something wrong with android event
 	if( finger > MAX_FINGERS )
 		return;
 
+	if( !( action >=0 && action <= 2 ) )
+		return;
 
 	x /= gWidth;
 	y /= gHeight;
@@ -294,6 +382,7 @@ void Java_in_celest_xash3d_XashActivity_nativeTouch(JNIEnv* env, jclass cls, jin
 		dx = dy = 0.0f;
 	events.fingers[finger].x = x, events.fingers[finger].y = y;
 
+	// check if we should skip some events
 	if( ( action == 2 ) && ( !dx || !dy ) )
 		return;
 
