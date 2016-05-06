ed8b3ea3a6eb80a20a43326c1ccb805f07ba28db
mittorn <mittorn@sibmail.com>
2016-05-06 14:55:00 +0000

    Add option to detect dladdr dynamicly

--- engine/common/library.c
+++ engine/common/library.c
@@ -182,6 +182,23 @@ void *Com_FunctionFromName( void *hInstance, const char *pName )
 	return function;
 }
 
+#ifdef XASH_DYNAMIC_DLADDR
+int d_dladdr( void *sym, Dl_info *info )
+{
+	static int (*dladdr_real) ( void *sym, Dl_info *info );
+
+	if( !dladdr_real )
+		dladdr_real = dlsym( (void*)(size_t)(-1), "dladdr" );
+
+	Q_memset( info, 0, sizeof( *info ) );
+
+	if( !dladdr_real )
+		return -1;
+
+	return dladdr_real(  sym, info );
+}
+#endif
+
 const char *Com_NameForFunction( void *hInstance, void *function )
 {
 #ifdef DLL_LOADER
