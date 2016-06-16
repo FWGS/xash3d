2e8fe0c42c5f3ebf6e698823cae8bd2e602ea2bc
mittorn <mittorn@sibmail.com>
2016-06-16 14:39:42 +0700

    Add missing macro

--- engine/common/sys_win.c
+++ engine/common/sys_win.c
@@ -673,7 +673,7 @@ void Sys_RestoreCrashHandler( void )
 #elif defined (CRASHHANDLER)
 // Posix signal handler
 #include "library.h"
-#if defined(__FreeBSD__) || defined(__NetBSD__)
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined __ANDROID__
 #define HAVE_UCONTEXT_H 1
 #endif
 
