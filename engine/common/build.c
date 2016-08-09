56bc4412a9cca2690a8854d7001f41af87792c88
a1batross <a1ba.omarov@gmail.com>
2016-08-09 23:31:38 +0600

    Fix build after previous commit

--- engine/common/build.c
+++ engine/common/build.c
@@ -126,7 +126,7 @@ Q_buildcommit will identify this build as release or "notset"
 const char *Q_buildcommit( void )
 {
 #ifdef XASH_BUILD_COMMIT
-	return XASH_BUILD_COMMIT
+	return XASH_BUILD_COMMIT;
 #elif defined(XASH_RELEASE) // don't check it elsewhere to avoid random bugs
 	return "release";
 #else
