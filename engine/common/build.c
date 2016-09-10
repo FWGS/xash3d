578c499baf26f8e4a29e2ac403f0d92b17607745
mittorn <mittorn@sibmail.com>
2016-09-10 20:01:18 +0000

    Bump version

--- engine/common/build.c
+++ engine/common/build.c
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 
 #include "common.h"
 
-//#define XASH_GENERATE_BUILDNUM
+#define XASH_GENERATE_BUILDNUM
 
 #if defined(XASH_GENERATE_BUILDNUM)
 static char *date = __DATE__ ;
