b46bd0801fa605dcfd0593537ff7c8c9de71bec2
mittorn <mittorn@sibmail.com>
2017-04-09 22:01:43 +0000

    Fix macro

--- engine/client/vgui/vgui_draw.c
+++ engine/client/vgui/vgui_draw.c
@@ -13,7 +13,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 */
 
-#ifdef XASH_VGUI
+#ifndef XASH_DEDICATED
 
 #include "common.h"
 #include "client.h"
