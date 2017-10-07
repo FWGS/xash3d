99120150981e236a0a4977bfdf0878aa153c3b32
Alibek Omarov (a1batross) <a1ba.omarov@gmail.com>
2017-10-08 02:02:44 +0300

    Reformat snd_sdl.c, remove dead code

--- engine/platform/sdl/snd_sdl.c
+++ engine/platform/sdl/snd_sdl.c
@@ -20,8 +20,8 @@ GNU General Public License for more details.
 
 #include <SDL.h>
 
-#define SAMPLE_16BIT_SHIFT		1
-#define SECONDARY_BUFFER_SIZE		0x10000
+#define SAMPLE_16BIT_SHIFT 1
+#define SECONDARY_BUFFER_SIZE 0x10000
 
 /*
 =======================================================================
@@ -29,27 +29,27 @@ Global variables. Must be visible to window-procedure function
 so it can unlock and free the data block after it has been played.
 =======================================================================
 */
-extern convar_t		*s_primary;
-extern dma_t			dma;
-
 static int sdl_dev;
 
 //static qboolean	snd_firsttime = true;
 //static qboolean	primary_format_set;
 
-void SDL_SoundCallback( void* userdata, Uint8* stream, int len)
+void SDL_SoundCallback( void *userdata, Uint8 *stream, int len )
 {
-	int size = dma.samples << 1;
-	int pos = dma.samplepos << 1;
+	int size    = dma.samples << 1;
+	int pos     = dma.samplepos << 1;
 	int wrapped = pos + len - size;
 
-	if (wrapped < 0) {
-		memcpy(stream, dma.buffer + pos, len);
+	if( wrapped < 0 )
+	{
+		memcpy( stream, dma.buffer + pos, len );
 		dma.samplepos += len >> 1;
-	} else {
+	}
+	else
+	{
 		int remaining = size - pos;
-		memcpy(stream, dma.buffer + pos, remaining);
-		memcpy(stream + remaining, dma.buffer, wrapped);
+		memcpy( stream, dma.buffer + pos, remaining );
+		memcpy( stream + remaining, dma.buffer, wrapped );
 		dma.samplepos = wrapped >> 1;
 	}
 }
@@ -68,48 +68,51 @@ qboolean SNDDMA_Init( void *hInst )
 
 	if( SDL_Init( SDL_INIT_AUDIO ) )
 	{
-		MsgDev( D_ERROR, "Audio: SDL: %s \n", SDL_GetError() );
+		MsgDev( D_ERROR, "Audio: SDL: %s \n", SDL_GetError( ) );
 		return false;
 	}
 
 #ifdef __linux__
-	setenv("PULSE_PROP_application.name", GI->title , 1);
-	setenv("PULSE_PROP_media.role", "game", 1);
+	setenv( "PULSE_PROP_application.name", GI->title, 1 );
+	setenv( "PULSE_PROP_media.role", "game", 1 );
 #endif
 
-	Q_memset(&desired, 0, sizeof(desired));
-	desired.freq = SOUND_DMA_SPEED;
-	desired.format = AUDIO_S16LSB;
-	desired.samples = 1024;
+	Q_memset( &desired, 0, sizeof( desired ) );
+	desired.freq     = SOUND_DMA_SPEED;
+	desired.format   = AUDIO_S16LSB;
+	desired.samples  = 1024;
 	desired.channels = 2;
 	desired.callback = SDL_SoundCallback;
 
 	sdl_dev = SDL_OpenAudioDevice( NULL, 0, &desired, &obtained, 0 );
 
-	if( !sdl_dev ) {
-		Con_Printf( "Couldn't open SDL audio: %s\n", SDL_GetError() );
+	if( !sdl_dev )
+	{
+		Con_Printf( "Couldn't open SDL audio: %s\n", SDL_GetError( ) );
 		return false;
 	}
 
-	if (obtained.format != AUDIO_S16LSB) {
-		Con_Printf("SDL audio format %d unsupported.\n", obtained.format);
+	if( obtained.format != AUDIO_S16LSB )
+	{
+		Con_Printf( "SDL audio format %d unsupported.\n", obtained.format );
 		goto fail;
 	}
 
-	if (obtained.channels != 1 && obtained.channels != 2) {
-		Con_Printf("SDL audio channels %d unsupported.\n", obtained.channels);
+	if( obtained.channels != 1 && obtained.channels != 2 )
+	{
+		Con_Printf( "SDL audio channels %d unsupported.\n", obtained.channels );
 		goto fail;
 	}
 
-	dma.format.speed = obtained.freq;
+	dma.format.speed    = obtained.freq;
 	dma.format.channels = obtained.channels;
-	dma.format.width = 2;
-	dma.samples = 0x8000 * obtained.channels;
-	dma.buffer = Z_Malloc(dma.samples * 2);
-	dma.samplepos = 0;
-	dma.sampleframes = dma.samples / dma.format.channels;
+	dma.format.width    = 2;
+	dma.samples         = 0x8000 * obtained.channels;
+	dma.buffer          = Z_Malloc( dma.samples * 2 );
+	dma.samplepos       = 0;
+	dma.sampleframes    = dma.samples / dma.format.channels;
 
-	Con_Printf("Using SDL audio driver: %s @ %d Hz\n", SDL_GetCurrentAudioDriver(), obtained.freq);
+	Con_Printf( "Using SDL audio driver: %s @ %d Hz\n", SDL_GetCurrentAudioDriver( ), obtained.freq );
 
 	SDL_PauseAudioDevice( sdl_dev, 0 );
 
@@ -117,7 +120,7 @@ qboolean SNDDMA_Init( void *hInst )
 	return true;
 
 fail:
-	SNDDMA_Shutdown();
+	SNDDMA_Shutdown( );
 	return false;
 }
 
@@ -144,32 +147,32 @@ update global soundtime
 */
 int SNDDMA_GetSoundtime( void )
 {
-	static int	buffers, oldsamplepos;
-	int		samplepos, fullsamples;
-	
+	static int buffers, oldsamplepos;
+	int samplepos, fullsamples;
+
 	fullsamples = dma.samples / 2;
 
 	// it is possible to miscount buffers
 	// if it has wrapped twice between
 	// calls to S_Update.  Oh well.
-	samplepos = SNDDMA_GetDMAPos();
+	samplepos = SNDDMA_GetDMAPos( );
 
 	if( samplepos < oldsamplepos )
 	{
 		buffers++; // buffer wrapped
 
 		if( paintedtime > 0x40000000 )
-		{	
+		{
 			// time to chop things off to avoid 32 bit limits
-			buffers = 0;
+			buffers     = 0;
 			paintedtime = fullsamples;
-			S_StopAllSounds();
+			S_StopAllSounds( );
 		}
 	}
 
 	oldsamplepos = samplepos;
 
-	return (buffers * fullsamples + samplepos / 2);
+	return ( buffers * fullsamples + samplepos / 2 );
 }
 
 /*
@@ -181,7 +184,7 @@ Makes sure dma.buffer is valid
 */
 void SNDDMA_BeginPainting( void )
 {
-	SDL_LockAudio();
+	SDL_LockAudio( );
 }
 
 /*
@@ -194,7 +197,7 @@ Also unlocks the dsound buffer
 */
 void SNDDMA_Submit( void )
 {
-	SDL_UnlockAudio();
+	SDL_UnlockAudio( );
 }
 
 /*
@@ -215,16 +218,16 @@ void SNDDMA_Shutdown( void )
 #ifndef __EMSCRIPTEN__
 		SDL_CloseAudioDevice( sdl_dev );
 #endif
-		SDL_CloseAudio();
+		SDL_CloseAudio( );
 	}
 
 	if( SDL_WasInit( SDL_INIT_AUDIO ) )
-		 SDL_QuitSubSystem( SDL_INIT_AUDIO );
+		SDL_QuitSubSystem( SDL_INIT_AUDIO );
 
 	if( dma.buffer )
 	{
-		 Mem_Free( dma.buffer );
-		 dma.buffer = NULL;
+		Mem_Free( dma.buffer );
+		dma.buffer = NULL;
 	}
 }
 
@@ -235,9 +238,8 @@ S_PrintDeviceName
 */
 void S_PrintDeviceName( void )
 {
-	Msg( "Audio: SDL (driver: %s)\n", SDL_GetCurrentAudioDriver() );
+	Msg( "Audio: SDL (driver: %s)\n", SDL_GetCurrentAudioDriver( ) );
 }
-
 
 /*
 ===========
