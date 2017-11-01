5895b0a8918dc340a10e63285eed63eec9400369
Alibek Omarov (a1batross) <a1ba.omarov@gmail.com>
2017-11-01 15:20:31 +0300

    Enable vectorized math calls for all autobuilds

--- scripts/build_osx_engine.sh
+++ scripts/build_osx_engine.sh
@@ -4,7 +4,18 @@
 
 cd $TRAVIS_BUILD_DIR
 mkdir -p osx-build && cd osx-build
-CFLAGS="-m32" CXXFLAGS="-m32" cmake -DXASH_SDL=yes -DXASH_VGUI=yes -DXASH_DLL_LOADER=no -DXASH_DEDICATED=no -DHL_SDK_DIR=../vgui-dev/ -DCMAKE_OSX_ARCHITECTURES=i386 -DMAINUI_USE_STB=yes -DXASH_AUTODETECT_SSE_BUILD=no ../
+export CFLAGS="-m32"
+export CXXFLAGS="-m32"
+cmake \
+	-DXASH_SDL=yes \
+	-DXASH_VGUI=yes \
+	-DXASH_DLL_LOADER=no \
+	-DXASH_DEDICATED=no \
+	-DHL_SDK_DIR=../vgui-dev/ \
+	-DCMAKE_OSX_ARCHITECTURES=i386 \
+	-DMAINUI_USE_STB=yes \
+	-DXASH_AUTODETECT_SSE_BUILD=no \
+	-DCMAKE_BUILD_TYPE=RelWithDebInfo ../
 make -j2
 cp engine/libxash.dylib mainui/libxashmenu.dylib vgui_support/libvgui_support.dylib ../vgui-dev/lib/vgui.dylib game_launch/xash3d ../xash3d.sh .
 cp ~/Library/Frameworks/SDL2.framework/SDL2 libSDL2.dylib
--- scripts/build_portable_linux_engine.sh
+++ scripts/build_portable_linux_engine.sh
@@ -3,7 +3,36 @@
 # Build custom SDL2
 
 cd $TRAVIS_BUILD_DIR/SDL2-2.0.6
-CC="ccache gcc -msse2 -march=i686 -m32 -ggdb -O2" ./configure --disable-dependency-tracking --enable-audio --enable-video --enable-events --disable-render --enable-joystick --disable-haptic --disable-power --enable-threads --enable-timers --enable-loadso --enable-video-opengl --enable-x11-shared --enable-video-x11 --enable-video-x11-xrandr --enable-video-x11-scrnsaver --enable-video-x11-xinput --disable-video-x11-xshape --disable-video-x11-xdbe --disable-libudev --disable-dbus --disable-ibus --enable-sdl-dlopen --disable-video-opengles --disable-cpuinfo --disable-assembly --disable-atomic --enable-alsa
+export CC="ccache gcc -msse2 -march=i686 -m32 -ggdb -O2"
+./configure \
+	--disable-dependency-tracking \
+	--enable-audio \
+	--enable-video \
+	--enable-events \
+	--disable-render \
+	--enable-joystick \
+	--disable-haptic \
+	--disable-power \
+	--enable-threads \
+	--enable-timers \
+	--enable-loadso \
+	--enable-video-opengl \
+	--enable-x11-shared \
+	--enable-video-x11 \
+	--enable-video-x11-xrandr \
+	--enable-video-x11-scrnsaver \
+	--enable-video-x11-xinput \
+	--disable-video-x11-xshape \
+	--disable-video-x11-xdbe \
+	--disable-libudev \
+	--disable-dbus \
+	--disable-ibus \
+	--enable-sdl-dlopen \
+	--disable-video-opengles \
+	--disable-cpuinfo \
+	--disable-assembly \
+	--disable-atomic \
+	--enable-alsa
 make -j2
 mkdir -p $TRAVIS_BUILD_DIR/sdl2-linux
 make install DESTDIR=$TRAVIS_BUILD_DIR/sdl2-linux
@@ -12,7 +41,19 @@ make install DESTDIR=$TRAVIS_BUILD_DIR/sdl2-linux
 
 cd $TRAVIS_BUILD_DIR
 mkdir -p build && cd build/
-CC="ccache gcc" CXX="ccache g++" CFLAGS="-m32" CXXFLAGS="-m32" cmake -DCMAKE_PREFIX_PATH=$TRAVIS_BUILD_DIR/sdl2-linux/usr/local -DXASH_STATIC=ON -DXASH_DLL_LOADER=ON -DXASH_VGUI=yes -DMAINUI_USE_STB=yes -DHL_SDK_DIR=$TRAVIS_BUILD_DIR/vgui-dev -DXASH_AUTODETECT_SSE_BUILD=OFF ../
+export CC="ccache gcc"
+export CXX="ccache g++"
+export CFLAGS="-m32"
+export CXXFLAGS="-m32"
+cmake \
+	-DCMAKE_PREFIX_PATH=$TRAVIS_BUILD_DIR/sdl2-linux/usr/local \
+	-DXASH_STATIC=ON \
+	-DXASH_DLL_LOADER=ON \
+	-DXASH_VGUI=ON \
+	-DMAINUI_USE_STB=ON \
+	-DHL_SDK_DIR=$TRAVIS_BUILD_DIR/vgui-dev \
+	-DXASH_AUTODETECT_SSE_BUILD=OFF \
+	-DCMAKE_BUILD_TYPE=RelWithDebInfo ../
 make -j2
 cp engine/xash mainui/libxashmenu.so vgui_support/libvgui_support.so vgui_support/vgui.so .
 cp $TRAVIS_BUILD_DIR/sdl2-linux/usr/local/lib/$(readlink $TRAVIS_BUILD_DIR/sdl2-linux/usr/local/lib/libSDL2-2.0.so.0) libSDL2-2.0.so.0
