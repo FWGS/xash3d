43655ea272bc92a277e2a6f77afc81dad53d3615
Alibek Omarov (a1batross) <a1ba.omarov@gmail.com>
2017-11-28 03:24:22 +0300

    Try to fix short changelog again

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
  echo "Travis should not deploy from pull requests"
  exit 0
@@ -16,7 +16,9 @@ else
  echo >> README.md
  echo Short changelog: >> README.md
  echo \`\`\` >> README.md
- (cd $TRAVIS_BUILD_DIR;git log --pretty=format:'%h %ad %s' --date iso -n 10 HEAD `curl https://raw.githubusercontent.com/FWGS/xash3d-deploy/$SOURCE_NAME-$TRAVIS_BRANCH/commit.txt`.. )| cut -d ' ' -f 1-3,5-100 >> README.md
+ pushd $TRAVIS_BUILD_DIR
+ git ---no-pager log --pretty=format:'%h %ad %s' --date iso -n 10 HEAD $(curl -s https://raw.githubusercontent.com/FWGS/xash3d-deploy/$SOURCE_NAME-$TRAVIS_BRANCH/commit.txt).. >> $OLDPWD/README.md
+ popd
  echo \`\`\` >> README.md
  echo >> README.md
  echo [Code on GitHub]\(https://github.com/FWGS/xash3d/tree/$TRAVIS_COMMIT\) >> README.md
