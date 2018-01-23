72f3e3ce4ce19cc4d7507370238e0b198e3ca4c5
Alibek Omarov <a1ba.omarov@gmail.com>
2018-01-24 01:35:49 +0300

    Fix mingw installing for travis

--- scripts/build_mingw_engine.sh
+++ scripts/build_mingw_engine.sh
@@ -18,7 +18,7 @@ cmake \
 	-DXASH_VGUI=ON \
 	-DXASH_SDL=ON ../
 make -j2
-make install DESTDIR=`pwd` # Install everything
+make install # Install everything
 cp $TRAVIS_BUILD_DIR/sdl2-mingw/i686-w64-mingw32/bin/SDL2.dll . # Install SDL2
 cp /usr/i686-w64-mingw32/lib/libwinpthread-1.dll . # a1ba: remove when travis will be updated to xenial
 7z a -t7z $TRAVIS_BUILD_DIR/xash3d-mingw.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on xash_sdl.exe menu.dll SDL2.dll vgui_support.dll libwinpthread-1.dll
