6cf35128d891fb0db3bb1f7fd0190d752e89eced
Alibek Omarov <a1ba.omarov@gmail.com>
2018-01-24 01:00:32 +0300

    Test cmake install and vgui downloading for MinGW

--- scripts/build_mingw_engine.sh
+++ scripts/build_mingw_engine.sh
@@ -15,9 +15,10 @@ cmake \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
 	-DCMAKE_PREFIX_PATH=../sdl2-mingw/i686-w64-mingw32 \
 	-DXASH_STATIC=ON \
-	-DXASH_VGUI=OFF \
+	-DXASH_VGUI=ON \
 	-DXASH_SDL=ON ../
 make -j2
-cp engine/xash_sdl.exe mainui/menu.dll $TRAVIS_BUILD_DIR/vgui_support_bin/vgui_support.dll $TRAVIS_BUILD_DIR/sdl2-mingw/i686-w64-mingw32/bin/SDL2.dll .
+make install DESTDIR=`pwd` # Install everything
+cp $TRAVIS_BUILD_DIR/sdl2-mingw/i686-w64-mingw32/bin/SDL2.dll . # Install SDL2
 cp /usr/i686-w64-mingw32/lib/libwinpthread-1.dll . # a1ba: remove when travis will be updated to xenial
 7z a -t7z $TRAVIS_BUILD_DIR/xash3d-mingw.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on xash_sdl.exe menu.dll SDL2.dll vgui_support.dll libwinpthread-1.dll
