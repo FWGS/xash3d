8e2240a0233f6a8ba2eb465856fe7405009c1140
Alibek Omarov <a1ba.omarov@gmail.com>
2018-01-27 16:43:18 +0300

    Fix OSX travis builds

--- scripts/build_osx_engine.sh
+++ scripts/build_osx_engine.sh
@@ -12,12 +12,16 @@ cmake \
 	-DXASH_VGUI=yes \
 	-DXASH_DLL_LOADER=no \
 	-DXASH_DEDICATED=no \
-	-DHL_SDK_DIR=../vgui-dev/ \
 	-DCMAKE_OSX_ARCHITECTURES=i386 \
 	-DMAINUI_USE_STB=yes \
 	-DXASH_VECTORIZE_SINCOS=yes \
-	-DCMAKE_BUILD_TYPE=RelWithDebInfo ../
+	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+	-DLIB_INSTALL_DIR=\"\" \
+	-DLIB_INSTALL_SUBDIR=\"\" \
+	-DXASH_NO_INSTALL_RUNSCRIPT=yes ../
 make -j2 VERBOSE=1
-cp engine/libxash.dylib mainui/libxashmenu.dylib vgui_support/libvgui_support.dylib ../vgui-dev/lib/vgui.dylib game_launch/xash3d ../scripts/xash3d.sh .
-cp ~/Library/Frameworks/SDL2.framework/SDL2 libSDL2.dylib
-tar -cjf $TRAVIS_BUILD_DIR/xash3d-osx.tar.bz2 libxash.dylib libxashmenu.dylib libvgui_support.dylib xash3d libSDL2.dylib xash3d.sh
+mkdir -p pkg/
+make install DESTDIR=pkg/
+cp ~/Library/Frameworks/SDL2.framework/SDL2 pkg/libSDL2.dylib
+cp ../xash3d.sh pkg/
+tar -cjf $TRAVIS_BUILD_DIR/xash3d-osx.tar.bz2 pkg/*
