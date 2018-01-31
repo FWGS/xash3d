5a74848a35a218adf301c10c19a41699a2029e4d
Alibek Omarov <a1ba.omarov@gmail.com>
2018-01-31 21:23:15 +0300

    Disable sound static channel dupe code from before prediction era

--- engine/client/s_main.c
+++ engine/client/s_main.c
@@ -361,7 +361,7 @@ channel_t *SND_PickStaticChannel( int entnum, sfx_t *sfx, const vec3_t pos )
 	channel_t	*ch = NULL;
 	int	i;
 
-#if 1
+#if 0 // a1ba: Removed as prediction is done enough to disable this
 	int dupe = 0;
 
 	// TODO: remove this code when predicting is will be done
