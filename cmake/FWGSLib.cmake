7a9bc1e060f7816e9a3e9df04294ba9b53608ace
Alibek Omarov <a1ba.omarov@gmail.com>
2018-02-14 22:51:18 +0300

    Force Visual Studio to use static libc

--- cmake/FWGSLib.cmake
+++ cmake/FWGSLib.cmake
@@ -250,3 +250,33 @@ macro(fwgs_add_compile_options lang)
 		set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} ${FLAGS_STR}")
 	endif()
 endmacro()
+
+# /*
+# ================
+# fwgs_fix_default_msvc_settings
+#
+# Tweaks CMake's default compiler/linker settings to suit Google Test's needs.
+#
+# This must be a macro(), as inside a function string() can only
+# update variables in the function scope.
+# ================
+# */
+macro(fwgs_fix_default_msvc_settings)
+	if (MSVC)
+		# For MSVC, CMake sets certain flags to defaults we want to override.
+		# This replacement code is taken from sample in the CMake Wiki at
+		# http://www.cmake.org/Wiki/CMake_FAQ#Dynamic_Replace.
+		foreach (flag_var
+        		CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+			CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+	        # When Google Test is built as a shared library, it should also use
+	        # shared runtime libraries.  Otherwise, it may end up with multiple
+	        # copies of runtime library data in different modules, resulting in
+	        # hard-to-find crashes. When it is built as a static library, it is
+	        # preferable to use CRT as static libraries, as we don't have to rely
+	        # on CRT DLLs being available. CMake always defaults to using shared
+	        # CRT libraries, so we override that default here.
+			string(REPLACE "/MD" "-MT" ${flag_var} "${${flag_var}}")
+		endforeach()
+	endif()
+endmacro()
