e3b5d55017c6b86fd5bff70cd9c3f94dfa29f6f3
mittorn <mittorn@sibmail.com>
2019-06-08 14:55:42 +0700

    scripts: ant: set java version to 1.6

--- scripts/build_android_engine.sh
+++ scripts/build_android_engine.sh
@@ -5,6 +5,6 @@ sh gen-version.sh travis build
 sh gen-config.sh test
 python2 makepak.py xash-extras assets/extras.pak
 ndk-build -j2 APP_CFLAGS="-w" NDK_CCACHE=ccache
-ant debug -Dtest.version=1
+ant debug -Dant.build.javac.target=1.6 -Dant.build.javac.source=1.6 -Dtest.version=1
 cp bin/xashdroid-debug.apk $TRAVIS_BUILD_DIR/xash3d-generic.apk
 tar cJf $TRAVIS_BUILD_DIR/android-debug-symbols.txz obj/local/*/*.so
